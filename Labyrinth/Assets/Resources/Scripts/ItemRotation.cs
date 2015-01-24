fe69c474b5463d6b19433bc4498bbf4437f5117f
jocowig <jocowig@gmail.com>
2015-01-24 16:46:33 -0500

    Added Item Rotation

---
+++ Labyrinth/Assets/Resources/Scripts/ItemRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemRotation : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+		setRotation ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(15,30,45)*Time.deltaTime);
+	}
+	void setRotation(){
+		transform.Rotate(45, 45, 45);
+	}
+}
