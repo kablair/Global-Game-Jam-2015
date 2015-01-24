48a7295c61d00269617b3ae6ca74ec88402a1193
Kelly Blair <kablair5@gmail.com>
2015-01-24 15:18:09 -0500

    Added item enum

--- Labyrinth/Assets/Resources/Scripts/ItemPickup.cs
+++ Labyrinth/Assets/Resources/Scripts/ItemPickup.cs
@@ -1,12 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class ItemPickup : MonoBehaviour
 {
+	enum Item
+	{
+		Item1, Rope
+
+
+	};
 	void OnCollisionEnter (Collision col)
 	{
-		if(col.gameObject.name=="Item1")
+		if(isItem (col.gameObject))
 			Destroy(col.gameObject);
-	
+
 	}
+
+	bool isItem(GameObject obj)
+	{
+		foreach (Item  item in ((Item[]) Item.GetValues(typeof(Item))))
+		{
+		        
+			string s= item.ToString();
+			if(s.Equals (obj.name))
+			{
+				return true;
+			}
+			
+		}
+		return false;
+	}
+
 }
