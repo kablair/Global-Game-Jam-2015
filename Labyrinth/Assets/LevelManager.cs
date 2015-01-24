8864e6919edb40409409ced0966bad235294ec2c
PhaZ90771 <shuttben@gmail.com>
2015-01-24 02:31:26 -0500

    Made stuff look better

--- Labyrinth/Assets/LevelManager.cs
+++ Labyrinth/Assets/LevelManager.cs
@@ -14,28 +14,31 @@ public class LevelManager : MonoBehaviour {
 		GameObject cameraPrefab = Resources.Load("Prefabs/Camera") as GameObject;
 
 		GameObject camera = GameObject.Instantiate(cameraPrefab) as GameObject;
-		camera.transform.position = new Vector3(10.0f, 10.0f, 10.0f);
-		camera.transform.LookAt(gameObject.transform);
+		camera.transform.position = new Vector3(5.0f, 10.0f, 5.0f);
+		camera.transform.Rotate(90.0f, 0.0f, 0.0f);
+		camera.name = "Main Camera";
+		//camera.transform.LookAt(gameObject.transform);
 	}
 
 	void SetupLevel() {
-		GameObject cubePrefab = Resources.Load("Prefabs/Cube") as GameObject;
+		GameObject wallPrefab = Resources.Load("Prefabs/Wall") as GameObject;
+		GameObject floorPrefab = Resources.Load("Prefabs/Floor") as GameObject;
 
-		GameObject cubeContainer = new GameObject();
-		cubeContainer.name = "Cube Container";
+		GameObject wallContainer = new GameObject();
+		wallContainer.name = "Wall Container";
 		Texture2D testLevel = Resources.Load ("TestLevel") as Texture2D;
 		for (int i = 0; i < testLevel.width; i++) {
 			for (int j = 0; j < testLevel.height; j++) {
 				Color color = testLevel.GetPixel(i, j);
 				if (color == Color.black) {
-					GameObject cube = GameObject.Instantiate(cubePrefab, new Vector3(i, 0.0f, j), new Quaternion()) as GameObject;
-					cube.transform.parent = cubeContainer.transform;
-					cube.name = "Cube";
+					GameObject wall = GameObject.Instantiate(wallPrefab, new Vector3(i, 0.0f, j), new Quaternion()) as GameObject;
+					wall.transform.parent = wallContainer.transform;
+					wall.name = "Cube";
 				}
 			}
 		}
 
-		GameObject floor = GameObject.Instantiate(cubePrefab) as GameObject;
+		GameObject floor = GameObject.Instantiate(floorPrefab) as GameObject;
 		floor.name = "Floor";
 		floor.transform.localScale = new Vector3(testLevel.width, 1.0f, testLevel.height);
 		floor.transform.position = new Vector3(testLevel.width / 2.0f - 0.5f, -1.0f, testLevel.height / 2.0f - 0.5f);
