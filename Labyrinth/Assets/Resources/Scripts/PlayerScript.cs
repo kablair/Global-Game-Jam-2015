38db46db620bb36c2db56a9e94268b047a46fd8b
Kelly Blair <kablair5@gmail.com>
2015-01-24 13:15:10 -0500

    Fixed pause

--- Labyrinth/Assets/Resources/Scripts/PlayerScript.cs
+++ Labyrinth/Assets/Resources/Scripts/PlayerScript.cs
@@ -10,10 +10,14 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
 		transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed);
 		transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed, Space.World);
+		if(!Paused.paused)
+		{
 		checkRotation ();
 		setSprintKey ();
+		}
 	}
 	void checkRotation(){
 		if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
