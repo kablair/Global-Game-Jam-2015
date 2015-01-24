3d8c8cf40f0dbb628c6e727dd5463830b192a887
jocowig <jocowig@gmail.com>
2015-01-24 02:24:49 -0500

    Added Player Movement
    Cube "Player" moves with wasd and arrow keys

---
+++ Labyrinth/Assets/PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float playerSpeed = 5.0f;
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed);
+		transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed, Space.World);
+	}
+}
