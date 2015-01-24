6df320e3b58d366933d78994a63d283a6fa08c1f
Kelly Blair <kablair5@gmail.com>
2015-01-24 17:10:53 -0500

    Revert "Added item enum"
    This reverts commit 48a7295c61d00269617b3ae6ca74ec88402a1193.

--- Labyrinth/Assets/Resources/Scripts/ItemPickup.cs
+++ Labyrinth/Assets/Resources/Scripts/ItemPickup.cs
@@ -1,35 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 public class ItemPickup : MonoBehaviour
 {
-	enum Item
-	{
-		Item1, Rope
-
-
-	};
 	void OnCollisionEnter (Collision col)
 	{
-		if(isItem (col.gameObject))
+		if(col.gameObject.name=="Item1")
 			Destroy(col.gameObject);
-
+	
 	}
-
-	bool isItem(GameObject obj)
-	{
-		foreach (Item  item in ((Item[]) Item.GetValues(typeof(Item))))
-		{
-		        
-			string s= item.ToString();
-			if(s.Equals (obj.name))
-			{
-				return true;
-			}
-			
-		}
-		return false;
-	}
-
 }
