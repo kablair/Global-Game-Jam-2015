9d666c6c4f09ad7d90808e16d6b21b12e46f6743
Kelly Blair <kablair5@gmail.com>
2015-01-25 04:31:01 -0500

    added some prefabs
    added some prefabs

---
+++ Labyrinth/Assets/Health.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	
+	public static void maxHealth=10;
+	public static void heath=maxHealth;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Labyrinth/Assets/Resources/Scripts/MonsterMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class MonsterMovement : MonoBehaviour {
+
+	GameObject target;
+//	int moveSpeed=3;
+//	int rotationSpeed=3;
+//	int range= 10f;
+//	int range2= 10f;
+//	int stop=0
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectsWithTag("Player")[0]; //target the player
+	}
+
+	void Update()
+	{	int distance = Vector3.Distance(.position, target.position);
+		if (distance<=range2 &&  distance>=range){
+			myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+			                                        Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
+		}
+		else if(distance<=range && distance>stop){
+			//move towards the player
+			myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+			                                        Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
+			myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+		}
+		else if (distance<=stop) {
+			myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+			                                        Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
+		}
+	}
+
+
+	
+}
