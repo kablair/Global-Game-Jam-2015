857d5db748c12964c1c116d598fd8c0e127aab70
Kelly Blair <kablair5@gmail.com>
2015-01-24 22:02:36 -0500

    added item enum

--- Labyrinth/Assets/Resources/Scripts/ItemSort.cs
+++ Labyrinth/Assets/Resources/Scripts/ItemSort.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class ItemSort : MonoBehaviour {
 
+	enum Item{
+		apple, feather, shield, staff, ashes, mirror
+	};
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +14,34 @@ public class ItemSort : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public static GameObject getPrefab(Item item)
+	{
+
+		if(item.Equals (Item.apple))
+		{
+			GameObject obj = Resources.Load("Prefabs/Apple") as GameObject;
+			return obj;
+		}
+		else if(item.Equals (Item.feather))
+		{
+			GameObject obj = Resources.Load("Prefabs/Feather") as GameObject;
+			return obj;
+		}
+	}
+
+	public static GameObject getImage(Item item)
+	{
+		if(item.Equals (Item.apple))
+		{
+			GameObject obj = Resources.Load("Prefabs/AppleImage") as GameObject;
+			return obj;
+		}
+		else if(item.Equals (Item.feather))
+		{
+			GameObject obj = Resources.Load("Prefabs/FeatherImage") as GameObject;
+			return obj;
+		}
 	}
 }
