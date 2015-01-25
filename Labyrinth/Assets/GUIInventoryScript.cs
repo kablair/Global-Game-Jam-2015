99b6bf2062f98c5fe109e55c9342f0df5c5a944c
jocowig <jocowig@gmail.com>
2015-01-25 13:59:36 -0500

    Inventory Working

--- Labyrinth/Assets/GUIInventoryScript.cs
+++ Labyrinth/Assets/GUIInventoryScript.cs
@@ -1,38 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GUIInventoryScript : MonoBehaviour {
 	string genString;
 	string itemString;
+	string extraString;
+	List<string> stringList;
 	// Use this for initialization
 	void Start () {
-	
+
+		stringList = new List<string>();
 	}
 	
 //	 Update is called once per frame
 	void Update () {
+		itemString = "Empty";
 		if (Inventory.newPickup) {
-			changeString();
-			itemString = Inventory.get ().name;
-			itemString = "Word";
+			string pickup = ItemPickup.pickupName;
+			stringList.Add(pickup);
+			Inventory.newPickup =false;
+//			extraString = Inventory.get ().name;
 				}
-		genString = Random.value.ToString();
+		genString = "Empty";
 	//	makeButtons ();
 	//	makeButtons ();
 	}
 	void OnGUI () {
-		itemString = "No Item";
 			makeButtons ();
 	}
 	void makeButtons(){// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(310,40,80,20), genString)) {
-
+		if(GUI.Button(new Rect(310,40,80,20), stringList[0])) {
 
 		}
 		
 		// Make the second button.
-		if(GUI.Button(new Rect(310,70,80,20), itemString)) {
+		if(GUI.Button(new Rect(310,70,80,20), stringList[1])) {
 
+		}
+		if(GUI.Button(new Rect(310,90,80,20), stringList[2])) {
+				
+		}
+
+		if(GUI.Button(new Rect(310,110,80,20), stringList[3])) {
+				
+		}
+		if(GUI.Button(new Rect(310,130,80,20), stringList[4])) {
+			
+		}
+		
+		if(GUI.Button(new Rect(310,150,80,20), stringList[5])) {
+			
 		}
 	}
 	string stringGen(){
