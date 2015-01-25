59b1c72247fbe3efd01b9a6a88ee198a3cbfa309
jocowig <jocowig@gmail.com>
2015-01-24 23:42:40 -0500

    Added Start Menu

---
+++ Labyrinth/Assets/StartMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		show();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void show(){
+				GetComponent<Canvas> ().enabled = true;
+		}
+}
