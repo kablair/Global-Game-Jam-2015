f60cb74a6588ab97d0dce6ee3d0d7009e024bc6d
jocowig <jocowig@gmail.com>
2015-01-25 02:34:41 -0500

    Added multiple assets, changed GUI

--- Labyrinth/Assets/Resources/Scripts/ItemPickup.cs
+++ Labyrinth/Assets/Resources/Scripts/ItemPickup.cs
@@ -4,6 +4,13 @@ using System.Collections;
 
 public class ItemPickup : MonoBehaviour
 {
+	Actionbar actionBar;
+	Sprite m_applePrefab;
+
+	void Start(){
+		m_applePrefab = Resources.Load ("Textures/ArrowTexture") as Sprite;
+	}
+
 	void OnCollisionEnter (Collision col)
 	{
 		if (isItem (col.gameObject)) {
@@ -12,6 +19,7 @@ public class ItemPickup : MonoBehaviour
 						//col.gameObject.transform.position=new Vector3(100,0,18);
 						//col.gameObject.transform.localScale+= new Vector3(0f,-0.98f,0f);
 						//stop item rotation
+			reportCollision(col.gameObject.name);
 						Destroy (col.gameObject);
 		}
 			else if (col.gameObject.name.Equals ("WallModel")) {
@@ -24,5 +32,9 @@ public class ItemPickup : MonoBehaviour
 		if (obj.tag.Equals("Item")) return true;
 		return false;
 	}
-
+	public void reportCollision( string name){
+				if (name.Equals("Apple")) {
+			//Sprite newSprite = Sprite.Create(Resources.Load("Textures/ArrowTexture"), new Rect(0,0,1,1), new Vector2(0.5f,0.5f),0);
+				}
+		}
 }
--- Labyrinth/Assets/UIScript.cs
+++ Labyrinth/Assets/UIScript.cs
@@ -14,6 +14,6 @@ public class UIScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		GetComponent(SpriteRenderer) = new Sprite();
 	}
 }
